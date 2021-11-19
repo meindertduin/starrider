d7eadb79530348392f51ff2145c56024e9dd86c4
Meindert van Duin <meindertvanduin99@gmail.com>
2021-11-19 21:41:01 +0100

    Test out cmouse functions and drawing methods in a short demo

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,34 @@ int main() {
     int white_color = WhitePixel(display, screen);
 
     window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 200, 100, 0, black_color, black_color);
-    XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask);
     XMapWindow(display , window);
 
     GC gc = XCreateGC(display, window, 0, nullptr);
+    XSetForeground(display, gc, white_color);
+    XSetBackground(display, gc,  black_color);
+    XSetFillStyle(display, gc, FillSolid);
+    XSetLineAttributes(display, gc, 2, LineSolid, CapRound, JoinRound);
+    XSync(display, false);
 
     for (;;) {
         XNextEvent(display, &event);
 
+        if (event.type == ButtonPress) {
+            int x = event.xbutton.x;
+            int y = event.xbutton.y;
+
+            switch (event.xbutton.button) {
+                case Button1:
+                    XDrawPoint(display, window, gc, x, y);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         if (event.type == Expose) {
-            XSetForeground(display, gc, white_color);
-            XFillRectangle(display, window, gc, 20, 20, 10, 10);
+            XFillRectangle(display, window, gc, 20, 20, 40, 40);
             XDrawLine(display, window, gc, 50, 50, 100, 100);
             XFlush(display);
         }
