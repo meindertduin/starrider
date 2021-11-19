a79da89af440e67de97540467deed12246aab22c
Meindert van Duin <meindertvanduin99@gmail.com>
2021-11-19 20:03:26 +0100

    Create a window and draw a line with X11

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
 #include <stdio.h>
+#include <X11/Xlib.h>
 
 int main() {
-    printf("hello world\n");
+    Display *display;
+    Window window;
+    XEvent event;
+    int screen;
+
+    display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        return 1;
+    }
+
+    screen = DefaultScreen(display);
+
+    int black_color = BlackPixel(display, screen);
+    int white_color = WhitePixel(display, screen);
+
+    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 200, 100, 0, black_color, black_color);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
+    XMapWindow(display , window);
+
+    GC gc = XCreateGC(display, window, 0, nullptr);
+
+    for (;;) {
+        XNextEvent(display, &event);
+
+        if (event.type == Expose) {
+            XSetForeground(display, gc, white_color);
+            XFillRectangle(display, window, gc, 20, 20, 10, 10);
+            XDrawLine(display, window, gc, 50, 50, 100, 100);
+            XFlush(display);
+        }
+
+        if (event.type == KeyPress) {
+            break;
+        }
+    }
+
+    XCloseDisplay(display);
+    return 0;
 }
