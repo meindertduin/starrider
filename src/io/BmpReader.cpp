48ca03c3728d3a770ba5fd9d1c6df4418226691c
Meindert van Duin <meindertvanduin99@gmail.com>
2021-11-26 21:37:51 +0100

    Read the file contents with the bmp reader

--- src/io/BmpReader.cpp
+++ src/io/BmpReader.cpp
@@ -6,11 +6,9 @@ BmpReader::~BmpReader() {
     if (m_ifs.is_open()) {
         m_ifs.close();
     }
-
-    delete[] p_pix_array;
 }
 
-size_t BmpReader::read_file(string path) {
+size_t BmpReader::read_file(string path, uint32_t *bitmap) {
     m_ifs = std::ifstream(path, std::ios::in | std::ios::binary);
 
     if (!m_ifs.is_open()) {
@@ -18,11 +16,15 @@ size_t BmpReader::read_file(string path) {
     }
 
     read_header();
+    bitmap = new uint32_t[m_header.bitmap_size / (m_header.bit_per_pixel / 8)];
 
-    printf("%d, %d\n", m_header.width, m_header.bit_per_pixel);
+    m_ifs.seekg(m_header.pix_array_offset, std::ios_base::beg);
+    m_ifs.read(reinterpret_cast<char*>(bitmap), m_header.bitmap_size);
 
-    return m_header.size;
+    return m_header.bitmap_size;
 }
+
+
 
 void BmpReader::read_header() {
     m_ifs.seekg(0x02, std::ios_base::beg);
@@ -37,4 +39,15 @@ void BmpReader::read_header() {
 
     m_ifs.seekg(0x1C, std::ios_base::beg);
     m_ifs.read(reinterpret_cast<char*>(&m_header.bit_per_pixel), sizeof(m_header.bit_per_pixel));
+
+    m_ifs.seekg(0x22, std::ios_base::beg);
+    m_ifs.read(reinterpret_cast<char*>(&m_header.bitmap_size), sizeof(m_header.bitmap_size));
+}
+
+uint32_t BmpReader::get_width() {
+    return m_header.width;
+}
+
+uint32_t BmpReader::get_height() {
+    return m_header.height;
 }
--- src/io/BmpReader.h
+++ src/io/BmpReader.h
@@ -10,7 +10,9 @@ public:
     BmpReader();
     ~BmpReader();
 
-    size_t read_file(string path);
+    size_t read_file(string path, uint32_t *bitmap);
+    uint32_t get_width();
+    uint32_t get_height();
 private:
     struct BmpHeader {
         uint32_t size;
@@ -18,9 +20,8 @@ private:
         uint32_t width;
         uint32_t height;
         u_int16_t bit_per_pixel;
+        uint32_t bitmap_size;
     } m_header;
-
-    char* p_pix_array;
 
     std::ifstream m_ifs;
 
