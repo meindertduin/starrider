5a49e22a7db89808461e5b24cf19be2dc6683fe8
Meindert van Duin <meindertvanduin99@gmail.com>
2021-12-09 23:05:47 +0100

    Make all the defenitions of the Subjects in the class itself

--- src/core/Events.h
+++ src/core/Events.h
@@ -12,53 +12,33 @@ class EventSubject;
 template<typename T>
 class EventObserver {
 public:
-    virtual ~EventObserver();
+    virtual ~EventObserver() {}
     virtual void on_event(const T &event) = 0;
 
 protected:
-    EventObserver();
+    EventObserver() {}
 };
 
 template<typename T>
 class EventSubject {
 public:
-    virtual ~EventSubject();
-    virtual void listen(EventObserver<T>* observer);
-    virtual void unlisten(EventObserver<T>* observer);
-    virtual void emit_event(const T &event);
+    virtual ~EventSubject() {}
+    virtual void listen(EventObserver<T>* observer) {
+        m_observers.push_back(observer);
+    }
+
+    virtual void unlisten(EventObserver<T>* observer) {
+        m_observers.remove(observer);
+    }
+
+    virtual void emit_event(const T &event) {
+        for (EventObserver<T>* observer : m_observers) {
+            observer->on_event(event);
+        }
+    }
 protected:
-    EventSubject();
+    EventSubject() {}
 
 private:
     std::list<EventObserver<T>*> m_observers;
 };
-
-template<typename T>
-EventObserver<T>::EventObserver() {}
-
-template<typename T>
-EventObserver<T>::~EventObserver() {}
-
-template<typename T>
-EventSubject<T>::EventSubject() {}
-
-template<typename T>
-EventSubject<T>::~EventSubject() {}
-
-template<typename T>
-void EventSubject<T>::listen(EventObserver<T>* observer) {
-    m_observers.push_back(observer);
-}
-
-template<typename T>
-void EventSubject<T>::unlisten(EventObserver<T>* observer) {
-    m_observers.remove(observer);
-}
-
-template<typename T>
-void EventSubject<T>::emit_event(const T &event) {
-    for (EventObserver<T>* observer : m_observers) {
-        observer->on_event(event);
-    }
-}
-
