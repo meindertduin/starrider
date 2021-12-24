61f3a227d123688cd90f56dacc948fd28bfbe6e2
Meindert van Duin <meindertvanduin99@gmail.com>
2021-12-24 21:03:49 +0100

    Add a fontset object for loading in the font

--- src/graphics/TTF.cpp
+++ src/graphics/TTF.cpp
@@ -1,10 +1,44 @@
-#include <ft2build.h>
-#include FT_FREETYPE_H
+#include <stdio.h>
+
+#include "TTF.h"
+
 
 FT_Library library;
 
 bool init_ttf() {
-    FT_Init_FreeType(&library);
+    auto error = FT_Init_FreeType(&library);
+    if (error) {
+        return false;
+    }
+
 
     return true;
+}
+
+FontSetTTF::FontSetTTF() {
+
+}
+
+bool FontSetTTF::load_font(std::string path) {
+    auto font_error = FT_New_Face(library, "/usr/share/fonts/TTF/Symbola.ttf", 0, &m_face);
+    if (font_error == FT_Err_Unknown_File_Format) {
+        return false;
+    }
+
+    FT_Set_Pixel_Sizes(m_face, 0, 48);
+
+    for (unsigned char c = 0; c < 128; c++) {
+        if (FT_Load_Char(m_face, c, FT_LOAD_RENDER)) {
+            printf("Error loading char: %c\n", c);
+            m_glyphs.clear();
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Glyph FontSetTTF::get_glyph(char c) {
+    return m_glyphs[c];
 }
--- src/graphics/TTF.h
+++ src/graphics/TTF.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <string>
+#include <map>
+
 bool init_ttf();
 
+struct Glyph {
+};
 
+class FontSetTTF {
+public:
+    FontSetTTF();
 
-
+    bool load_font(std::string path);
+    Glyph get_glyph(char c);
+private:
+    FT_Face m_face;
+    std::map<char, Glyph> m_glyphs;
+};
