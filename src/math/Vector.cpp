a1ee1e5f726894a6838fede83a6a841d5c242ed7
Meindert van Duin <meindertvanduin99@gmail.com>
2022-01-15 17:15:29 +0100

    Add essential vector functions
    The functions need to still be optimized, for example getting the length which is
    done quite alot can be implemented faster with for example Taylor
    series approximation.

---
+++ src/math/Vector.cpp
@@ -0,0 +1,87 @@
+#include "Vector.h"
+#include <math.h>
+#include "Core.h"
+
+namespace Math {
+    float V2D_Type::length() const {
+        return std::sqrt(x * x + y * y);
+    }
+
+    V2D_Type& V2D_Type::normalise() {
+        float l = length();
+        x /= l; y /= l;
+
+        return *this;
+    }
+
+    V2D_Type V2D_Type::normalized() const {
+        float l = length();
+        V2D_Type r { x / l, y / l };
+        return r;
+    }
+
+
+    float V2D_Type::cos_th(const V2D_Type &other) const {
+        return fast_cos(dot(other) / (length() * other.length()));
+    }
+
+    V3D_Type V3D_Type::cross(const V3D_Type &other) const {
+        V3D_Type r;
+        r.x = y * other.z - z * other.y;
+        r.y = z * other.x - x * other.z;
+        r.z = x * other.y - y * other.x;
+
+        return r;
+    }
+
+    float V3D_Type::length() const {
+        return sqrtf(x * x + y * y + z * z);
+    }
+
+    V3D_Type& V3D_Type::normalise() {
+        float l = length();
+        x /= l; y /= l; z /= l;
+
+        return *this;
+    }
+
+    V3D_Type V3D_Type::normalized() const {
+        float l = length();
+        V3D_Type r { x / l, y / l, z / l };
+        return r;
+    }
+
+    float V3D_Type::cos_th(const V3D_Type &other) const {
+        return fast_cos(dot(other) / (length() * other.length()));
+    }
+
+    V4D_Type V4D_Type::cross(const V4D_Type &other) const {
+        V4D_Type r;
+        r.x = y * other.z - z * other.y;
+        r.y = z * other.x - x * other.z;
+        r.z = x * other.y - y * other.x;
+        r.w = 1.0f;
+
+        return r;
+    }
+
+    float V4D_Type::length() const {
+        return sqrtf(x * x + y * y + z * z + w * w);
+    }
+
+    V4D_Type& V4D_Type::normalise() {
+        float l = length();
+        x /= l; y /= l; z /= l;
+        return *this;
+    }
+
+    V4D_Type V4D_Type::normalized() const {
+        float l = length();
+        V4D_Type r { x / l, y / l, z / l };
+        return r;
+    }
+
+    float V4D_Type::cos_th(const V4D_Type &other) const {
+        return fast_cos(dot(other) / (length() * other.length()));
+    }
+}
--- src/math/Vector.h
+++ src/math/Vector.h
@@ -12,6 +12,7 @@ typedef struct V2D_Type {
 
     V2D_Type() = default;
     constexpr V2D_Type(float x, float y) : x(x), y(y) {  }
+    constexpr V2D_Type(const V2D_Type &init, const V2D_Type &term) : x(term.x - init.x), y(term.y - init.y) {}
 
     V2D_Type operator+(const V2D_Type &rhs) const {
         V2D_Type r;
@@ -82,8 +83,7 @@ typedef struct V2D_Type {
     }
 
     float length() const;
-    float length_fast() const;
-    void normalise();
+    V2D_Type& normalise();
     V2D_Type normalized() const;
     float cos_th(const V2D_Type &other) const;
 
@@ -98,7 +98,9 @@ typedef struct V3D_Type {
     };
 
     V3D_Type() = default;
-    V3D_Type(float x, float y, float z) : x(x), y(y), z(z) {  }
+    constexpr V3D_Type(float x, float y, float z) : x(x), y(y), z(z) {  }
+    constexpr V3D_Type(const V3D_Type &init, const V3D_Type &term)
+        : x(term.x - init.x), y(term.y - init.y), z(term.z - init.z) {}
 
     V3D_Type operator+(const V3D_Type &rhs) const {
         V3D_Type r;
@@ -178,8 +180,7 @@ typedef struct V3D_Type {
 
     V3D_Type cross(const V3D_Type &other) const;
     float length() const;
-    float length_fast() const;
-    void normalise();
+    V3D_Type& normalise();
     V3D_Type normalized() const;
     float cos_th(const V3D_Type &other) const;
 
@@ -193,8 +194,11 @@ typedef struct V4D_Type {
         };
     };
 
-    V4D_Type() = default;
+    constexpr V4D_Type() : x(0), y(0), z(0), w(1) {  };
+    constexpr V4D_Type(float x, float y, float z) : x(x), y(y), z(z), w(1) {  }
     constexpr V4D_Type(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {  }
+    constexpr V4D_Type(const V4D_Type &init, const V4D_Type &term)
+        : x(term.x - init.x), y(term.y - init.y), z(term.z - init.z), w(1) {}
 
     V4D_Type operator+(const V4D_Type &rhs) const {
         V4D_Type r;
@@ -282,8 +286,7 @@ typedef struct V4D_Type {
 
     V4D_Type cross(const V4D_Type &other) const;
     float length() const;
-    float length_fast() const;
-    void normalise();
+    V4D_Type& normalise();
     V4D_Type normalized() const;
     float cos_th(const V4D_Type &other) const;
 
