674523920ea602b5d6f2b8c5eec6bd0987ae349d
Meindert van Duin <meindertvanduin99@gmail.com>
2022-01-18 21:00:44 +0100

    ADd basic back-face culling

--- src/graphics/RenderPipeline.cpp
+++ src/graphics/RenderPipeline.cpp
@@ -13,22 +13,27 @@ void RenderPipeline::render_viewport(const Camera &camera, std::vector<Renderabl
     m_rasterizer.clear_depth_buffer();
 
     auto vp = camera.get_view_projection();
+    auto v_forward = camera.m_transform.rot.get_forward();
 
     for (auto renderable : renderables) {
         Matrix4x4 transform = renderable.transform->get_matrix_transformation();
         for (auto triangle : renderable.mesh->triangles) {
             Triangle translated_tri;
-            translated_tri.p[0] = triangle.p[0].transform(transform);
-            translated_tri.p[1] = triangle.p[1].transform(transform);
-            translated_tri.p[2] = triangle.p[2].transform(transform);
 
-            Triangle proj_tri;
+            auto dp = translated_tri.p[0].normal.dot(v_forward);
+            if (dp > 0) {
+                translated_tri.p[0] = triangle.p[0].transform(transform);
+                translated_tri.p[1] = triangle.p[1].transform(transform);
+                translated_tri.p[2] = triangle.p[2].transform(transform);
 
-            proj_tri.p[0] = translated_tri.p[0].transform(vp, transform);
-            proj_tri.p[1] = translated_tri.p[1].transform(vp, transform);
-            proj_tri.p[2] = translated_tri.p[2].transform(vp, transform);
+                Triangle proj_tri;
 
-            m_rasterizer.draw_triangle(proj_tri, *renderable.mesh->texture);
+                proj_tri.p[0] = translated_tri.p[0].transform(vp, transform);
+                proj_tri.p[1] = translated_tri.p[1].transform(vp, transform);
+                proj_tri.p[2] = translated_tri.p[2].transform(vp, transform);
+
+                m_rasterizer.draw_triangle(proj_tri, *renderable.mesh->texture);
+            }
         }
     }
 }
