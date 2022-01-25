2636a492362925730c65a75be364e84e3cb8b307
Meindert van Duin <meindertvanduin99@gmail.com>
2022-01-25 20:46:25 +0100

    Remove rasterization to the core, to prepare for rebuild

--- src/graphics/Rasterizer.cpp
+++ src/graphics/Rasterizer.cpp
@@ -8,32 +8,6 @@ Rasterizer::~Rasterizer() {
 }
 
 void Rasterizer::draw_triangle(Triangle &triangle, const Texture &texture) {
-     std::vector<Vertex> vertices;
-     std::vector<Vertex> auxilary_list;
-
-     vertices.push_back(triangle.p[0]);
-     vertices.push_back(triangle.p[1]);
-     vertices.push_back(triangle.p[2]);
-
-     // Add image space clipping
-     if (clip_polygon_axis(vertices, auxilary_list, 0) &&
-             clip_polygon_axis(vertices, auxilary_list, 1) &&
-             clip_polygon_axis(vertices, auxilary_list, 2))
-     {
-         Vertex initial_vertex = vertices[0];
-
-         for (int i = 1; i < static_cast<int>(vertices.size()) - 1; i++) {
-             Triangle draw_triangle;
-             draw_triangle.p[0] = initial_vertex;
-             draw_triangle.p[1] = vertices[i];
-             draw_triangle.p[2] = vertices[i + 1];
-
-             fill_triangle(draw_triangle, texture);
-         }
-     }
-}
-
-void Rasterizer::fill_triangle(Triangle &triangle, const Texture &texture) {
     auto screen_space = Math::mat_4x4_screen_space((float)m_width / 2.0f, (float)m_height / 2.0f);
     auto identity = Math::Mat_4x4_Identity;
 
@@ -70,77 +44,30 @@ void Rasterizer::fill_triangle(Triangle &triangle, const Texture &texture) {
 
     bool handedness =  (x1 * y2 - x2 * y1) >= 0.0f;
 
-	scan_triangle(min_y_vert, mid_y_vert, max_y_vert, handedness, texture);
+    Edge bottom_to_top = Edge(min_y_vert, max_y_vert, 0);
+    Edge bottom_to_middle = Edge(min_y_vert, mid_y_vert, 0);
+    Edge middle_to_top = Edge(mid_y_vert, max_y_vert, 1);
+
+    scan_edges(bottom_to_top, bottom_to_middle, handedness, texture);
+    scan_edges(bottom_to_top, middle_to_top, handedness, texture);
 }
 
 void Rasterizer::clear_depth_buffer() {
     std::fill(p_z_buffer, p_z_buffer + m_width * m_height, INFINITY);
 }
 
-inline void Rasterizer::scan_triangle(const Vertex &min_y_vert, const Vertex &mid_y_vert, const Vertex &max_y_vert, bool handedness, const Texture &texture) {
-    Gradients gradients = Gradients(min_y_vert, mid_y_vert, max_y_vert);
-
-    Edge bottom_to_top = Edge(min_y_vert, max_y_vert, gradients, 0);
-    Edge bottom_to_middle = Edge(min_y_vert, mid_y_vert, gradients, 0);
-    Edge middle_to_top = Edge(mid_y_vert, max_y_vert, gradients, 1);
-
-    scan_edges(bottom_to_top, bottom_to_middle, handedness, texture, gradients);
-    scan_edges(bottom_to_top, middle_to_top, handedness, texture, gradients);
-}
-
-inline void Rasterizer::scan_edges(Edge &a, Edge &b, bool handedness, const Texture &texture, const Gradients &gradients) {
+inline void Rasterizer::scan_edges(Edge &a, Edge &b, bool handedness, const Texture &texture) {
     int y_start = b.y_start;
-    int y_end   = b.y_end;
+    int y_end = b.y_end;
 
     for(int j = y_start; j < y_end; j++) {
         if (handedness) {
-            draw_scanline(b, a, j, texture, gradients);
+            draw_scanline(b, a, j, texture);
         } else {
-            draw_scanline(a, b, j, texture, gradients);
+            draw_scanline(a, b, j, texture);
         }
         a.step();
         b.step();
-    }
-}
-
-inline void Rasterizer::draw_scanline(const Edge &left, const Edge &right, int j, const Texture &texture, const Gradients &gradients) {
-    int x_min = std::ceil(left.x);
-    int x_max = std::ceil(right.x);
-    float x_prestep = (float)x_min - left.x;
-
-    float x_dist = right.x - left.x;
-    float text_coord_x_xstep = gradients.text_coord_x_xstep;
-    float text_coord_y_xstep = gradients.text_coord_y_xstep;
-    float one_over_zx_step = gradients.one_over_zx_step;
-    float depth_x_step = gradients.depth_x_step;
-    float light_amount_xstep = gradients.light_amount_xstep;
-
-    float text_coord_x = left.text_coord_x + text_coord_x_xstep * x_prestep;
-    float text_coord_y = left.text_coord_y + text_coord_x_xstep * x_prestep;
-    float one_over_z = left.one_over_z + one_over_zx_step * x_prestep;
-    float depth = left.depth - depth_x_step * x_prestep;
-    float light_amount = left.light_amount + gradients.light_amount_xstep * x_prestep;
-
-    for(int i = x_min; i < x_max; i++)
-    {
-        int index = i + j * m_width;
-        if (p_z_buffer[index] > depth) {
-            float z = 1.0f / one_over_z;
-
-            int src_x = (text_coord_x * z) * (texture.width - 1) + 0.5f;
-            int src_y = (text_coord_y * z) * (texture.height - 1) + 0.5f;
-
-            uint32_t value = texture.get_pixel(src_x, src_y).value;
-
-            p_framebuffer[m_width * j + i].value = value;
-            p_z_buffer[index] = depth;
-        }
-
-        text_coord_x += text_coord_x_xstep;
-        text_coord_y += text_coord_y_xstep;
-        one_over_z += one_over_zx_step;
-        depth += depth_x_step;
-        light_amount += light_amount_xstep;
     }
 }
 
@@ -153,51 +80,5 @@ void Rasterizer::set_viewport(int width, int height) {
 
         p_z_buffer = new float[width * height];
         p_framebuffer = p_renderer->get_framebuffer();
-    }
-}
-
-inline bool Rasterizer::clip_polygon_axis(std::vector<Vertex> &vertices, std::vector<Vertex> &auxilary_list, int component_index) {
-    clip_polygon_component(vertices, component_index, 1.0f, auxilary_list);
-    vertices.clear();
-
-    if(auxilary_list.empty())
-    {
-        return false;
-    }
-
-    clip_polygon_component(auxilary_list, component_index, -1.0f, vertices);
-    auxilary_list.clear();
-
-    return !vertices.empty();
-}
-
-inline void Rasterizer::clip_polygon_component(std::vector<Vertex> &vertices, int component_index,
-    float component_factor, std::vector<Vertex> &result)
-{
-    Vertex prev_vertex = vertices[vertices.size() -1];
-    float previous_component = prev_vertex.get(component_index) * component_factor;
-    bool previous_inside = previous_component <= prev_vertex.pos.w;
-
-    for (auto current_vertex : vertices) {
-        float current_component = current_vertex.get(component_index) * component_factor;
-        bool current_inside = current_component <= current_vertex.pos.w;
-
-        if((current_inside || previous_inside) && current_inside != previous_inside)
-        {
-            float lerpAmt = (prev_vertex.pos.w - previous_component) /
-                ((prev_vertex.pos.w - previous_component) -
-                 (current_vertex.pos.w - current_component));
-
-            result.push_back(prev_vertex.lerp(current_vertex, lerpAmt));
-        }
-
-        if(current_inside)
-        {
-            result.push_back(current_vertex);
-        }
-
-        prev_vertex = current_vertex;
-        previous_component = current_component;
-        previous_inside = current_inside;
     }
 }
--- src/graphics/Rasterizer.h
+++ src/graphics/Rasterizer.h
@@ -4,104 +4,14 @@
 #include "Renderer.h"
 #include "Texture.h"
 
-
-struct Gradients {
-    V4D text_coords[3];
-    float one_over_z[3];
-    float depth[3];
-    float light_amount[3];
-
-    float text_coord_x_xstep;
-    float text_coord_x_ystep;
-
-    float text_coord_y_xstep;
-    float text_coord_y_ystep;
-
-    float one_over_zx_step;
-    float one_over_zy_step;
-
-    float depth_x_step;
-    float depth_y_step;
-    float light_amount_xstep;
-    float light_amount_ystep;
-
-    Gradients(const Vertex& min_y_vert, const Vertex mid_y_vert, const Vertex max_y_vert) {
-		float oneOverdX = 1.0f /
-			(((mid_y_vert.pos.x - max_y_vert.pos.x) *
-			(min_y_vert.pos.y - max_y_vert.pos.y)) -
-			((min_y_vert.pos.x - max_y_vert.pos.x) *
-			(mid_y_vert.pos.y - max_y_vert.pos.y)));
-
-		float oneOverdY = -oneOverdX;
-
-        depth[0] = min_y_vert.pos.z;
-        depth[1] = mid_y_vert.pos.z;
-        depth[2] = max_y_vert.pos.z;
-
-        V4D light_dir = V4D(0, 0, -1);
-        light_amount[0] = saturate(min_y_vert.normal.dot(light_dir)) * 0.9f + 0.1f;
-        light_amount[1] = saturate(mid_y_vert.normal.dot(light_dir)) * 0.9f + 0.1f;
-        light_amount[2] = saturate(max_y_vert.normal.dot(light_dir)) * 0.9f + 0.1f;
-
-        // The z value is the occlusion z value.
-        one_over_z[0] = 1.0f / min_y_vert.pos.w;
-        one_over_z[1] = 1.0f / mid_y_vert.pos.w;
-        one_over_z[2] = 1.0f / max_y_vert.pos.w;
-
-        // The w value is the perspective z value.
-		text_coords[0] = min_y_vert.text_coords * one_over_z[0];
-		text_coords[1] = mid_y_vert.text_coords * one_over_z[1];
-		text_coords[2] = max_y_vert.text_coords * one_over_z[2];
-
-        auto calc_x_step = [&](float f0, float f1, float f2) {
-            return (((f1 - f2) * (min_y_vert.pos.y - max_y_vert.pos.y)) -
-                ((f0 - f2) * (mid_y_vert.pos.y - max_y_vert.pos.y))) * oneOverdX;
-        };
-
-        auto calc_y_step = [&](float f0, float f1, float f2) {
-            return (((f1 - f2) * (min_y_vert.pos.x - max_y_vert.pos.x)) -
-                ((f0 - f2) * (mid_y_vert.pos.x - max_y_vert.pos.x))) * oneOverdY;
-        };
-
-        text_coord_x_xstep = calc_x_step(text_coords[0].x, text_coords[1].x, text_coords[2].x);
-        text_coord_x_ystep = calc_y_step(text_coords[0].x, text_coords[1].x, text_coords[2].x);
-
-        text_coord_y_xstep = calc_x_step(text_coords[0].y, text_coords[1].y, text_coords[2].y);
-        text_coord_y_ystep = calc_y_step(text_coords[0].y, text_coords[1].y, text_coords[2].y);
-
-        one_over_zx_step = calc_x_step(one_over_z[0], one_over_z[1], one_over_z[2]);
-        one_over_zy_step = calc_y_step(one_over_z[0], one_over_z[1], one_over_z[2]);
-
-        depth_x_step = calc_x_step(depth[0], depth[1], depth[2]);
-        depth_y_step = calc_y_step(depth[0], depth[1], depth[2]);
-
-        light_amount_xstep = calc_x_step(light_amount[0], light_amount[1], light_amount[2]);
-        light_amount_ystep = calc_y_step(light_amount[0], light_amount[1], light_amount[2]);
-    }
-};
-
 struct Edge {
     float x;
     float x_step;
     int y_start;
     int y_end;
 
-    float text_coord_x;
-    float text_coord_xstep;
-    float text_coord_y;
-    float text_coord_ystep;
-
-    float one_over_z;
-    float one_over_zstep;
-
-    float depth;
-    float depth_step;
-
-    float light_amount;
-    float light_amount_step;
-
     Edge() {}
-    Edge(const Vertex &min_y_vert, const Vertex &max_y_vert, const Gradients &gradients, int min_y_vert_index) {
+    Edge(const Vertex &min_y_vert, const Vertex &max_y_vert, int min_y_vert_index) {
     	y_start = (int)std::ceil(min_y_vert.pos.y);
 		y_end = (int)std::ceil(max_y_vert.pos.y);
 
@@ -113,38 +23,10 @@ struct Edge {
 		x = min_y_vert.pos.x + y_prestep * x_step;
 
         float x_prestep = x - min_y_vert.pos.x;
-
-        text_coord_x = gradients.text_coords[min_y_vert_index].x +
-            gradients.text_coord_x_xstep * x_prestep + gradients.text_coord_x_ystep * y_prestep;
-        text_coord_xstep = gradients.text_coord_x_ystep + gradients.text_coord_x_xstep * x_step;
-
-        text_coord_y = gradients.text_coords[min_y_vert_index].y +
-            gradients.text_coord_y_xstep * x_prestep + gradients.text_coord_y_ystep * y_prestep;
-        text_coord_ystep = gradients.text_coord_y_ystep + gradients.text_coord_y_xstep * x_step;
-
-        one_over_z = gradients.one_over_z[min_y_vert_index] +
-            gradients.one_over_zx_step * x_prestep +
-            gradients.one_over_zy_step * y_prestep;
-        one_over_zstep = gradients.one_over_zy_step + gradients.one_over_zx_step * x_step;
-
-        depth = gradients.depth[min_y_vert_index] +
-            gradients.depth_x_step * x_prestep +
-            gradients.depth_y_step * y_prestep;
-        depth_step = gradients.depth_y_step + gradients.depth_x_step * x_step;
-
-        light_amount = gradients.light_amount[min_y_vert_index] +
-            gradients.light_amount_xstep * x_prestep +
-            gradients.light_amount_ystep * y_prestep;
-        light_amount_step = gradients.light_amount_ystep + gradients.light_amount_xstep * x_step;
     }
 
     constexpr void step() {
         x += x_step;
-        text_coord_x += text_coord_xstep;
-        text_coord_y += text_coord_ystep;
-        one_over_z += one_over_zstep;
-        depth += depth_step;
-        light_amount += light_amount_step;
     }
 };
 
@@ -153,7 +35,6 @@ public:
     Rasterizer(Renderer* renderer);
     ~Rasterizer();
     void draw_triangle(Triangle &triangle, const Texture &texture);
-    void fill_triangle(Triangle &triangle, const Texture &texture);
     void clear_depth_buffer();
     void set_viewport(int width, int height);
 private:
@@ -163,9 +44,16 @@ private:
 
     Pixel* p_framebuffer;
 
-    inline void scan_triangle(const Vertex &min_y_vert, const Vertex &mid_y_vert, const Vertex &max_y_vert, bool handedness, const Texture &texture);
-    inline void scan_edges(Edge &a, Edge &b, bool handedness, const Texture &texture, const Gradients &gradients);
-    inline void draw_scanline(const Edge &left, const Edge &right, int j, const Texture &texture, const Gradients &gradients);
-    inline void clip_polygon_component(std::vector<Vertex> &vertices, int component_index, float component_factor, std::vector<Vertex> &result);
-    inline bool clip_polygon_axis(std::vector<Vertex> &vertices, std::vector<Vertex> &auxilary_list, int component_index);
+    inline void scan_edges(Edge &a, Edge &b, bool handedness, const Texture &texture);
+    void draw_scanline(const Edge &left, const Edge &right, int y, const Texture &texture) {
+        int x_min = std::ceil(left.x);
+
+        int x_max = std::ceil(right.x);
+
+        for(int x = x_min; x < x_max; x++)
+        {
+            if (x > 0 && y > 0 && x < m_width && y < m_height)
+                p_framebuffer[m_width * y + x].value = 0x00ff0000;
+        }
+    }
 };
