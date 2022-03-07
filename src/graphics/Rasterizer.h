f0e8b19255f434278b9bd6a35a3d04e562dbf81f
Meindert van Duin <meindertvanduin99@gmail.com>
2022-03-07 18:57:01 +0100

    Add comments explaining all the rasterization external functions

--- src/graphics/Rasterizer.h
+++ src/graphics/Rasterizer.h
@@ -93,10 +93,16 @@ void draw_colored_gouraud_triangle(RenderListPoly &poly);
 
 ////////// Perfect perspective texture mapping //////////
 
+// Draws triangle with perfect perspective texture mapping with flat shading and using inverse z-buffering.
 void draw_perspective_textured_triangle_fsinvzb(RenderListPoly &poly);
+
+// Draws triangle with perfect perspective texture mapping with intensity gourad shading and using inverse z-buffering.
 void draw_perspective_textured_triangle_iinvzb(RenderListPoly &poly);
 
+// Draws triangle with perfect perspective texture mapping with flat shading and using inverse z-buffering.
 void draw_perspective_textured_triangle_fs(RenderListPoly &poly);
+
+// Draws triangle with perfect perspective texture mapping with intensity gourad shading and using inverse z-buffering.
 void draw_perspective_textured_triangle_i(RenderListPoly &poly);
 
 ////////// Piecewise perspective texture mapping //////////
@@ -109,25 +115,42 @@ void draw_piecewise_textured_triangle_iinvzb(RenderListPoly &poly);
 
 // Draws triangle with piecewise perspective texture mapping with flat shading and using inverse z-buffering.
 void draw_piecewise_textured_triangle_fs(RenderListPoly &poly);
-//
+
 // Draws triangle with piecewise perspective texture mapping with intensity gourad shading and using inverse z-buffering.
 void draw_piecewise_textured_triangle_i(RenderListPoly &poly);
 
 ////////// Affine texture mapping //////////
 
+// Draws triangle with affine texture mapping with flat shading and using inverse z-buffering.
 void draw_affine_textured_triangle_fsinvzb(RenderListPoly &poly);
+
+// Draws triangle with affine texture mapping with intensity gourad shading and using inverse z-buffering.
 void draw_affine_textured_triangle_iinvzb(RenderListPoly &poly);
 
+// Draws triangle with affine texture mapping with flat shading and using inverse z-buffering.
 void draw_affine_textured_triangle_fs(RenderListPoly &poly);
+
+// Draws triangle with affine texture mapping with intensity gourad shading and using inverse z-buffering.
 void draw_affine_textured_triangle_i(RenderListPoly &poly);
 
-// Non-textured rastization methods
+////////// Non-textured rastization methods //////////
+
+// Draws a triangle using solid shading.
 void draw_triangle_s(RenderListPoly &poly);
+
+// Draws a triangle using flat shading.
 void draw_triangle_fs(RenderListPoly &poly);
+
+// Draws a triangle using intensity gourad shading.
 void draw_triangle_i(RenderListPoly &poly);
 
+// Draws a triangle using solid shading and using inverse z-buffering.
 void draw_triangle_sinvzb(RenderListPoly &poly);
+
+// Draws a triangle using flat shading and using inverse z-buffering.
 void draw_triangle_fsinvzb(RenderListPoly &poly);
+
+// Draws a triangle using intensity gourad shading and using inverse z-buffering.
 void draw_triangle_iinvzb(RenderListPoly &poly);
 
 void scan_edges(CGouradEdge &left, CGouradEdge &right, bool handedness, A565Color color, const RenderListPoly &poly);
