10dba909c13412ae1bef0d51abc506dc9ba1c557
Meindert van Duin <meindertvanduin99@gmail.com>
2022-03-22 19:50:06 +0100

    Only do Obj file content reading in the obj-reader

--- src/io/ObjReader.cpp
+++ src/io/ObjReader.cpp
@@ -65,40 +65,29 @@ bool ObjReader::read_file(string path, int text_width, int text_height) {
     return true;
 }
 
-void ObjReader::create_render_object(RenderObject &object, Texture *texture) {
-    object.state = ObjectStateActive | ObjectStateVisible;
+ObjFileContent ObjReader::extract_content() {
+    auto result = ObjFileContent {};
 
-    // TODO: read if object has multiple frames out of the file
-    object.frames_count = 1;
-    object.curr_frame = 0;
-    object.attributes |= ObjectAttributeSingleFrame;
+    result.vertex_count = m_vertices.size();
+    result.text_count = m_tex_coords.size();
 
-    object.vertex_count = m_vertices.size();
-    object.text_count = m_tex_coords.size();
+    result.vertices = new Vertex4D[result.vertex_count];
+    result.text_coords = new Point2D[m_tex_coords.size()];
 
-    object.local_vertices = new Vertex4D[object.vertex_count];
-    object.transformed_vertices = new Vertex4D[object.vertex_count];
-    object.texture_coords = new Point2D[m_tex_coords.size()];
-
-    object.head_local_vertices = &object.local_vertices[0];
-    object.head_transformed_vertices = &object.transformed_vertices[0];
-
-    object.color = A565Color(0xFF, 0, 0, 0);
-
-    for (int i = 0; i < object.vertex_count; i++) {
-        object.local_vertices[i].v = m_vertices[i];
-        object.local_vertices[i].attributes = VertexAttributePoint;
+    for (int i = 0; i < result.vertex_count; i++) {
+        result.vertices[i].v = m_vertices[i];
+        result.vertices[i].attributes = VertexAttributePoint;
     }
 
-    for (int i = 0; i < object.text_count; i++) {
-        object.texture_coords[i] = m_tex_coords[i];
+    for (int i = 0; i < result.text_count; i++) {
+        result.text_coords[i] = m_tex_coords[i];
     }
 
     std::vector<Polygon> polygons;
     for (int i = 0; i < m_indices.size(); i += 3) {
         Polygon polygon;
-        polygon.vertices = object.local_vertices;
-        polygon.text_coords = object.texture_coords;
+        polygon.vertices = result.vertices;
+        polygon.text_coords = result.text_coords;
 
         // TODO: get these values from obj file
         polygon.state = PolyStateActive;
@@ -115,7 +104,7 @@ void ObjReader::create_render_object(RenderObject &object, Texture *texture) {
             polygon.vert[j] = current_index.vertex_index;
 
             if (has_tex_coords) {
-                polygon.vertices[current_index.vertex_index].t = object.texture_coords[current_index.tex_coord_index];
+                polygon.vertices[current_index.vertex_index].t = result.text_coords[current_index.tex_coord_index];
                 polygon.text[j] = current_index.tex_coord_index;
 
                 polygon.vertices[current_index.vertex_index].attributes |= VertexAttributeTexture;
@@ -130,35 +119,36 @@ void ObjReader::create_render_object(RenderObject &object, Texture *texture) {
         if (polygon.attributes & PolyAttributeShadeModeGouraud ||
                 polygon.attributes & PolyAttributeShadeModeIntensityGourad) {
 
-            auto line1 = object.transformed_vertices[polygon.vert[0]].v
-                - object.transformed_vertices[polygon.vert[1]].v;
+            auto line1 = result.vertices[polygon.vert[0]].v
+                - result.vertices[polygon.vert[1]].v;
 
-            auto line2 = object.transformed_vertices[polygon.vert[0]].v
-                - object.transformed_vertices[polygon.vert[2]].v;
+            auto line2 = result.vertices[polygon.vert[0]].v
+                - result.vertices[polygon.vert[2]].v;
 
             polygon.n_length = line1.cross(line2).length();
-            polygon.text_coords = object.texture_coords;
+            polygon.text_coords = result.text_coords;
         }
 
-        // TODO read color out of file
-        polygon.color = object.color;
+        // polygon.color = object.color;
+
         polygons.push_back(polygon);
     }
 
-
-    object.poly_count = polygons.size();
-    object.polygons = new Polygon[object.poly_count];
+    result.poly_count = polygons.size();
+    result.polygons = new Polygon[result.poly_count];
 
     for (int i = 0; i < polygons.size(); i++) {
-        object.polygons[i] = polygons[i];
+        result.polygons[i] = polygons[i];
     }
 
     if (!has_normal_indices) {
-        compute_vertex_normals(object);
+        compute_vertex_normals(result);
     }
+
+    return result;
 }
 
-int ObjReader::compute_vertex_normals(RenderObject &object) {
+int ObjReader::compute_vertex_normals(ObjFileContent &object) {
     int polys_touch_vertices[ObjectMaxVertices];
     memset((void*)polys_touch_vertices, 0, sizeof(int) * ObjectMaxVertices);
 
@@ -168,11 +158,11 @@ int ObjReader::compute_vertex_normals(RenderObject &object) {
             int vi1 = object.polygons[poly].vert[1];
             int vi2 = object.polygons[poly].vert[2];
 
-            auto line1 = object.local_vertices[vi0].v
-                - object.local_vertices[vi1].v;
+            auto line1 = object.vertices[vi0].v
+                - object.vertices[vi1].v;
 
-            auto line2 = object.local_vertices[vi0].v
-                - object.local_vertices[vi2].v;
+            auto line2 = object.vertices[vi0].v
+                - object.vertices[vi2].v;
 
             auto n = line1.cross(line2);
 
@@ -182,16 +172,16 @@ int ObjReader::compute_vertex_normals(RenderObject &object) {
             polys_touch_vertices[vi1]++;
             polys_touch_vertices[vi2]++;
 
-            object.local_vertices[vi0].n += n;
-            object.local_vertices[vi1].n += n;
-            object.local_vertices[vi2].n += n;
+            object.vertices[vi0].n += n;
+            object.vertices[vi1].n += n;
+            object.vertices[vi2].n += n;
         }
     }
 
     for (int vertex = 0; vertex < object.vertex_count; vertex++) {
         if (polys_touch_vertices[vertex] >= 1) {
-            object.local_vertices[vertex].n /= polys_touch_vertices[vertex];
-            object.local_vertices[vertex].n.normalise();
+            object.vertices[vertex].n /= polys_touch_vertices[vertex];
+            object.vertices[vertex].n.normalise();
         }
     }
 
--- src/io/ObjReader.h
+++ src/io/ObjReader.h
@@ -16,10 +16,22 @@ struct ObjIndex {
     int normal_index;
 };
 
+struct ObjFileContent {
+    int vertex_count;
+    Vertex4D *vertices;
+
+    int text_count;
+    Point2D *text_coords;
+
+    int poly_count;
+    Polygon *polygons;
+};
+
 class ObjReader {
 public:
     ObjReader();
     bool read_file(string path, int text_width, int text_height);
+    ObjFileContent extract_content();
     void create_render_object(RenderObject &object, Texture *texture);
 private:
     vector<V4D> m_vertices;
@@ -31,5 +43,5 @@ private:
     bool has_normal_indices = false;
 
     ObjIndex parse_object_index(string token);
-    int compute_vertex_normals(RenderObject &object);
+    int compute_vertex_normals(ObjFileContent &object);
 };
