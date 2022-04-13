d1d0982062bbdf187b9c1527589be45f27291567
Meindert van Duin <meindertvanduin99@gmail.com>
2022-04-13 23:57:44 +0200

    Read all the file paths in a direcotry and store them

--- tools/obj_converter/main.cpp
+++ tools/obj_converter/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <exception>
+
+#include "../../src/io/ObjReader.h"
+
+int convert_obj_files(std::string dir);
 
 int main(int argc, char *argv[]) {
+    std::vector<std::string> argument_inputs;
+
     for (int i = 0; i < argc; i++)
-        std::cout << argv[i] << "\n";
+        argument_inputs.push_back(std::string(argv[i]));
+
+    convert_obj_files(argument_inputs[1]);
+
+    return 0;
+}
+
+int convert_obj_files(std::string path) {
+    std::vector<std::string> file_paths;
+
+    try {
+        for (const auto &entry : std::filesystem::directory_iterator(path))
+            file_paths.push_back(entry.path().string());
+    } catch(std::filesystem::filesystem_error error) {
+        printf("Could not open directory. Are you sure the directory exists?\n");
+    }
 
     return 0;
 }
